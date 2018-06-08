db5777c4f0b593a32823a9a5c8b8173f69cca31f
tanbro <tanbro@163.com>
2018-06-08 18:22:25 +0800

    version: set a POST version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     use_scm_version={
         # guess-next-dev:	automatically guesses the next development version (default)
         # post-release:	generates post release versions (adds postN)
-        'version_scheme': 'guess-next-dev',
+        'version_scheme': 'post-release',
     },
     setup_requires=['pytest-runner', 'setuptools_scm', 'setuptools_scm_git_archive'],
 
