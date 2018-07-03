9c47d7e03da9c73893e77e5d6c8c475406f5db5b
tanbro <tanbro@163.com>
2018-07-03 14:02:46 +0800

    fuck

--- setup.py
+++ setup.py
@@ -44,5 +44,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Markup',
     ],
 )
