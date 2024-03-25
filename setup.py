c685612c3ad2a972bb86f3491a9976729b210d67
tanbro <tanbro@163.com>
2024-03-25 21:57:53 +0800

    change: disable PEP518

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+
+setup(
+    use_scm_version={"write_to": "src/yamlinclude/version.py"},
+)
--- src/yamlinclude/__init__.py
+++ src/yamlinclude/__init__.py
@@ -4,4 +4,3 @@ Include YAML files within YAML
 
 from .constructor import *
 from .readers import *
-from .version import __version__, __version_tuple__
