eaa47657d2e34a8fdb156f04696a8d3328af17d0
tanbro <tanbro@163.com>
2024-03-25 22:11:47 +0800

    change: remove setuptools-scm

--- src/yamlinclude/__init__.py
+++ src/yamlinclude/__init__.py
@@ -4,3 +4,4 @@ Include YAML files within YAML
 
 from .constructor import *
 from .readers import *
+from .version import __version__, __version_tuple__
---
+++ src/yamlinclude/version.py
@@ -0,0 +1,2 @@
+__version__ = "1.4.0"
+__version_tuple__ = version_tuple = (1, 4, 0)
