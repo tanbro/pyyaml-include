d6362b56f1db6914d6f02c841d47a0669c25e967
tanbro <tanbro@163.com>
2024-03-25 17:41:13 +0800

    tests: improve compose tests

--- tests/README.md
+++ tests/README.md
@@ -1,24 +1,18 @@
 # How to run unit-tests
 
-1. install the project in editable mode, and also install it's tests dependencies:
+To run test cases, we can either:
 
-   ```bash
-   cd ${workspaceFolder}
-   python -m pip install -e . -r tests/requirements.txt
-   ```
+* Run tests in local/virtual-env Python environment directly
 
-1. To run test cases, we can either:
+  ```bash
+  cd ${workspaceFolder}
+  python -m pip install -e . -r tests/requirements.txt
+  python -m unittest
+  ```
 
-   * Run tests in local/virtual-env Python environment directly
+* Run multiple python versions tests with docker compose
 
-     ```bash
-     cd ${workspaceFolder}
-     python -m unittest
-     ```
-
-   * Run multiple python versions tests with docker compose
-
-        ```bash
-        cd ${workspaceFolder}/tests
-        docker compose up
-        ```
+  ```bash
+  cd ${workspaceFolder}/tests
+  docker compose up
+  ```
---
+++ tests/run-compose-test.sh
@@ -0,0 +1,25 @@
+set -e
+
+export SETUPTOOLS_SCM_PRETEND_VERSION=0
+export PIP_ROOT_USER_ACTION=ignore
+export PIP_NO_WARN_SCRIPT_LOCATION=1
+
+PYTHON_LIST=(python3.8 python3.9 python3.10 python3.11 python3.12)
+for PYTHON in ${PYTHON_LIST[@]}
+do
+    echo "---------------------------------------------------------------"
+    echo "Begin of ${PYTHON} unit-test"
+    echo "---------------------------------------------------------------"
+    echo
+    TMPDIR=$(mktemp -d)
+    trap 'rm -rf $TMPDIR' EXIT
+    $PYTHON -m venv $TMPDIR
+    $TMPDIR/bin/python -m pip install -U -e ./ -r tests/requirements.txt coverage
+    $TMPDIR/bin/python -m coverage run -m unittest -cfv
+    $TMPDIR/bin/python -m coverage report
+    echo
+    echo "---------------------------------------------------------------"
+    echo "End of ${PYTHON} unit-test"
+    echo "---------------------------------------------------------------"
+    echo
+done
