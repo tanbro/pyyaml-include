fed9ccaa9e1b1060523003ea36efe5c8f9d1b897
tanbro <tanbro@163.com>
2024-03-26 22:55:08 +0800

    small modifications

--- tests/run-compose-test.sh
+++ tests/run-compose-test.sh
@@ -8,19 +8,20 @@ export PIP_NO_WARN_SCRIPT_LOCATION=1
 PYTHON_LIST=(python3.8 python3.9 python3.10 python3.11 python3.12)
 for PYTHON in ${PYTHON_LIST[@]}
 do
-    echo "---------------------------------------------------------------"
+    echo
+    echo "================================================================"
     echo "Begin of ${PYTHON} unit-test"
-    echo "---------------------------------------------------------------"
+    echo "================================================================"
     echo
     TMPDIR=$(mktemp -d)
     trap 'rm -rf $TMPDIR' EXIT
     $PYTHON -m venv $TMPDIR
-    $TMPDIR/bin/python -m pip install -U -e ./ -r tests/requirements.txt coverage
+    $TMPDIR/bin/python -m pip install -e ./ -r tests/requirements.txt coverage
     $TMPDIR/bin/python -m coverage run -m unittest -cfv
     $TMPDIR/bin/python -m coverage report
     echo
-    echo "---------------------------------------------------------------"
+    echo "*****************************************************************"
     echo "End of ${PYTHON} unit-test"
-    echo "---------------------------------------------------------------"
+    echo "*****************************************************************"
     echo
 done
