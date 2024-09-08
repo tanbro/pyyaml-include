92f922313ac719bdb7932ad1837cb5923ad36476
Liu Xue Yan <liu_xue_yan@foxmail.com>
2024-09-08 16:40:47 +0800

    New Feature: Support Flattening Sequence Objects When Including Multiple Files Added the flatten parameter, which allows flattening sequence objects when including multiple YAML files. This is applicable when top-level sequence objects are included in each matching YAML file, avoiding the creation of a two-dimensional array and instead providing a flattened sequence.
    By setting flatten=true, users can merge sequence objects from multiple included files into a single sequence rather than having a sequence of sequences. If the flatten parameter is not set or is set to false (the default behavior), the sequences will remain nested as they were originally.
    
    This feature is only available when multiple matching files are included, and each matching file must contain a sequence object at the top level; otherwise, it may trigger a TypeError exception.

--- src/yaml_include/data.py
+++ src/yaml_include/data.py
@@ -20,6 +20,52 @@ class Data:
         Using the ``"**"`` pattern in large directory trees or remote files may consume an inordinate amount of time.
     """
 
+    flatten: bool = False
+    """Whether to flatten sequence object pared from multiple matched YAML files.
+
+    * Only available when multiple files were matched
+    * **Every matched file should have a Sequence object in its top level**, or a :class:`TypeError` exception may be thrown.
+
+    Example:
+        Consider we have such a YAML:
+
+        .. code-block:: yaml
+
+            items: !include "*.yaml"
+
+        If every file matches `*.yaml` contains a sequence object at the top level in it, what parsed and loaded will be:
+
+        .. code-block:: yaml
+
+                items: [
+                    [item 0 of 1st file, item 1 of 1st file, ... , item n of 1st file, ...],
+                    [item 0 of 2nd file, item 1 of 2nd file, ... , item n of 2nd file, ...],
+                    # ....
+                    [item 0 of nth file, item 1 of nth file, ... , item n of nth file, ...],
+                    # ...
+                ]
+
+        It's a 2-dim array, because YAML content of each matched file is treated as a member of the list(sequence).
+
+        But if ``flatten`` parameter was set to ``true``, like:
+
+        .. code-block:: yaml
+
+            items: !include {urlpath: "*.yaml", flatten: true}
+
+        we'll get:
+
+            .. code-block:: yaml
+
+                items: [
+                    item 0 of 1st file, item 1 of 1st file, ... , item n of 1st file,  # ...
+                    item 0 of 2nd file, item 1 of 2nd file, ... , item n of 2nd file,  # ...
+                    # ....
+                    item 0 of n-th file, item 1 of n-th file, ... , item n of n-th file,  # ...
+                    # ...
+                ]
+    """
+
     sequence_params: Sequence[Any] = field(default_factory=list)
     """sequence parameters of the YAML include statement.
     """
