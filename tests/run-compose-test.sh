83a0be39c84d1eb47a566ead84cdb5e1d96707bd
Liu Xue Yan <liu_xue_yan@foxmail.com>
2024-11-09 17:08:03 +0800

    ci: update mypy to v1.13.0 and add PyPy support
    - Update mypy version from v1.12.1 to v1.13.0 in pre-commit configuration
    - Add PyPy3.9 and PyPy3.10 to the list of Python versions for testing

--- tests/run-compose-test.sh
+++ tests/run-compose-test.sh
@@ -5,7 +5,7 @@ export PIP_DISABLE_PIP_VERSION_CHECK=1
 export PIP_ROOT_USER_ACTION=ignore
 export PIP_NO_WARN_SCRIPT_LOCATION=1
 
-PYTHON_LIST=(python3.8 python3.9 python3.10 python3.11 python3.12 python3.13)
+PYTHON_LIST=(python3.8 python3.9 python3.10 python3.11 python3.12 python3.13 pypy3.9 pypy3.10)
 for PYTHON in ${PYTHON_LIST[@]}
 do
     echo
